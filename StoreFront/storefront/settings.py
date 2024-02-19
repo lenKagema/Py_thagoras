cb2b1baf12f115005137e3f285006ad801e61cae
lenKagema <leninkagema@gmail.com>
2024-02-19 20:58:46 +0300

    Add steps to writig views in django

--- StoreFront/storefront/settings.py
+++ StoreFront/storefront/settings.py
@@ -34,9 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.sessions', Not used in modern APIs.
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground'
 ]
 
 MIDDLEWARE = [
