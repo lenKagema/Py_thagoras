a8d8049cad59cd5e4d3cc77f0abd2c0eaabc30b7
lenKagema <leninkagema@gmail.com>
2024-02-23 22:33:54 +0300

    Create templates directory and html file

--- CodeMarket/market.py
+++ CodeMarket/market.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/index')
+def home_page():
+    return render_template('index.html')
