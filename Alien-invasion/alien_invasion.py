ea3398b0a3845d5d7e4f974f5aa988d1003ccfa2
lenKagema <leninkagema72@gmail.com>
2024-02-25 20:38:50 +0300

    Add feature for bullets in the game

--- Alien-invasion/alien_invasion.py
+++ Alien-invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -16,15 +17,19 @@ def run_game():
     # Make a ship.
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in.
+    bullets = Group()
+
     # Set the background color.
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
         # Watch out for keyboard and  mouse events.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
