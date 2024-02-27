e45ed6bf57d988830c814d4af0fc6d059bda92f2
lenKagema <leninkagema72@gmail.com>
2024-02-27 21:22:42 +0300

    Create new template html page and add new decorator for that template

--- CodeMarket/market.py
+++ CodeMarket/market.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def home_page():
     return render_template('index.html')
+
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html')
