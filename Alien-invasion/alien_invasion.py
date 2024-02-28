7ba88e933db7325a7bb4d96f2a6327ae43f0e318
lenKagema <leninkagema72@gmail.com>
2024-02-28 21:50:33 +0300

    Create the alien fleet feature

--- Alien-invasion/alien_invasion.py
+++ Alien-invasion/alien_invasion.py
@@ -15,14 +15,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
-    # Make a ship.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
 
     # Make an alien.
     alien = Alien(ai_settings, screen)
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Set the background color.
     bg_color = (230, 230, 230)
@@ -33,7 +35,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- Alien-invasion/game_functions.py
+++ Alien-invasion/game_functions.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from bullet import Bullet
+from alien import Alien
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -34,20 +35,25 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
-    """Respond to key-presses and mouse events."""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+def create_fleet(ai_settings, screen, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien and find the number of aliens in a row.
+    # Spacing between each alien is equal to one alien width.
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
 
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen, ship, bullets)
+    # Create the first row of aliens.
+    for alien_number in range(number_aliens_x):
+        # Create an alien and place it in the row.
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
 
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event, ship)
 
-
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass of the loop.
     screen.fill(ai_settings.bg_color)
@@ -55,7 +61,7 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
@@ -71,3 +77,16 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove()
     print(len(bullets))
+
+
+def check_events(ai_settings, screen, ship, bullets):
+    """Respond to key-presses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
