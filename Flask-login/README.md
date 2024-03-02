b490830dc3ae0c44aad00c5608e3c27d7cb4a2e5
lenKagema <leninkagema72@gmail.com>
2024-03-02 17:45:32 +0300

    Push complete login application

--- Flask-login/README.md
+++ Flask-login/README.md
@@ -2,3 +2,4 @@
 
 Welcome to a web login authentication system written in the python flask framework.
 Credit goes to the author. Find link attached below:
+https://github.com/arpanneupane19/Python-Flask-Authentication-Tutorial/tree/main
--- Flask-login/app.py
+++ Flask-login/app.py
@@ -8,10 +8,10 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 login_manager = LoginManager()
@@ -52,15 +52,47 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+
+    return render_template('login.html', form=form)
 
 
-@app.route('/register')
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
