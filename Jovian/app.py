57a7097356d342c7ce587f9b74fab5741c0b5d8c
lenKagema <leninkagema72@gmail.com>
2024-03-07 21:06:00 +0300

    Initialize pipenv for project and modify templates folder

--- Jovian/app.py
+++ Jovian/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,25 +6,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
-class Job(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(20), nullable=False)
-    location = db.Column(db.String(30), nullable=False)
-    salary = db.Column(db.String(30), nullable=False)
+# class Job(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     title = db.Column(db.String(20), nullable=False)
+#     location = db.Column(db.String(30), nullable=False)
+#     salary = db.Column(db.String(30), nullable=False)
+
+jobs = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Remote',
+        'salary': 'Rs. 1,200,000'
+    }
+]
 
 
 @app.route('/')
 def home_page():
-    jobs = Job.query.all()
-    # jobs = [
-    #     {
-    #         'id': 1,
-    #         'title': 'Data Analyst',
-    #         'location': 'Remote',
-    #         'salary': 'Rs. 1,200,000'
-    #     }
-    # ]
-    return render_template('home.html', jobs=jobs)
+    # jobs = Job.query.all()
+    return render_template('home.html', jobs=jobs, company_name='Jovian')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(jobs)
 
 
 if __name__ == '__main__':
