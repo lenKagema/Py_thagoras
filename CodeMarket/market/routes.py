0797e4655f1c093e2f1b5452a8a7fdb30974f872
lenKagema <leninkagema72@gmail.com>
2024-03-11 21:02:49 +0300

    Add forn functionality to the app

--- CodeMarket/market/routes.py
+++ CodeMarket/market/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from . import app
 from .models import Item
+from .forms import RegisterForm
 
 
 @app.route('/')
@@ -13,3 +14,9 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
+
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
