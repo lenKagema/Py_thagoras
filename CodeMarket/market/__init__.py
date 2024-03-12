4b933e03d7cba2bfa1441250b117b19886c3e4bb
lenKagema <leninkagema72@gmail.com>
2024-03-12 21:15:49 +0300

    Add register page with form functionality

--- CodeMarket/market/__init__.py
+++ CodeMarket/market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'c7fbc86d44bc46d8862d7836'
 db = SQLAlchemy(app)
 
 from . import models
