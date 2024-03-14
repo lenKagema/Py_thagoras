d3ddd158f5e91e5757707b64a7e696389300b182
lenKagema <leninkagema72@gmail.com>
2024-03-14 20:55:15 +0300

    Initialize flask form validation

--- CodeMarket/market/routes.py
+++ CodeMarket/market/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
-from . import app
-from .models import Item
+from flask import render_template, redirect, url_for
+from . import app, db
+from .models import Item, User
 from .forms import RegisterForm
 
 
@@ -19,4 +19,11 @@ def market_page():
 @app.route('/register')
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+
     return render_template('register.html', form=form)
