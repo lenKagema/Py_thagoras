32999c25703fe24731e477170f4c8bb0cebbd1d6
lenKagema <leninkagema72@gmail.com>
2024-03-19 19:18:07 +0300

    Add UserMixin functionality to project

--- CodeMarket/market/models.py
+++ CodeMarket/market/models.py
@@ -1,7 +1,13 @@
-from . import db, bcrypt
+from . import db, bcrypt, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
